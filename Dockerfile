48ffb013471f22eb86f73ff4b87572516b3bc182
Jomarc0 <rayjomar15@gmail.com>
2026-05-13 23:36:29 +0800

    railway

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,9 @@ COPY src ./src
 
 RUN ./mvnw clean package -DskipTests
 
+COPY start.sh ./start.sh
+RUN chmod +x ./start.sh
+
 EXPOSE 8080
 
 CMD ["java", "-Xmx256m", "-jar", "target/premier-0.0.1-SNAPSHOT.jar"]
--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ mkdir -p /etc/secrets
 echo "$DIALOGFLOW_CREDENTIALS_JSON" > /etc/secrets/dialogflow-service-account.json
 echo "$FIREBASE_CREDENTIALS_JSON"   > /etc/secrets/firebase-service-account.json
 
-java -jar /app/target/premier-0.0.1-SNAPSHOT.jar
+java -Xmx256m -jar target/premier-0.0.1-SNAPSHOT.jar
