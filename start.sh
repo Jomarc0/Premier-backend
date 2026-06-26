041a0f45b5c5ff4c5d2b612bb36e910bddb14893
Jomarc0 <rayjomar15@gmail.com>
2026-06-26 08:20:15 +0800

    remove railway

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 
-mkdir -p /etc/secrets
-echo "$DIALOGFLOW_CREDENTIALS_JSON" > /etc/secrets/dialogflow-service-account.json
-echo "$FIREBASE_CREDENTIALS_JSON"   > /etc/secrets/firebase-service-account.json
-
 java -Xmx256m -jar target/premier-0.0.1-SNAPSHOT.jar
